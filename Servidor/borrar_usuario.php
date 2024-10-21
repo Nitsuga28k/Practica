ceb2241320e8f1378ebf51c9e7b185ab505568a4
Nitsuga28k <l21240045@smartin.tecnm.mx>
2024-10-20 20:48:47 -0600

    act

--- Servidor/borrar_usuario.php
+++ Servidor/borrar_usuario.php
@@ -4,6 +4,6 @@ if(!empty($_GET['id'])){
     $clave=$_GET['id'];
     $consulta=mysqli_query($conexion, "DELETE FROM usuarios WHERE idusu = $clave");
     mysqli_close($conexion);
-    header('Location: ../Cliente/Uusaurios.php');    
+    header('Location: ../Cliente/Uusaurios.php')  
 }
 ?>
